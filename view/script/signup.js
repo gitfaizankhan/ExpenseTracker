1b74e9f53fce3b8f90f1d46d06096112a52b8170
gitfaizankhan <faizankhaninfo9@gmail.com>
2023-03-12 01:13:40 +0530

    first commit

---
+++ view/script/signup.js
@@ -0,0 +1,17 @@
+async function signup(){
+    try{
+        let name = document.getElementById('name').value;
+        let email = document.getElementById('email').value;
+        let pass = document.getElementById('password').value;
+
+        const signupData = {
+            name: name,
+            email: email,
+            password: pass
+        }
+        const data = await axios.post('http://localhost:3000/user/signup', signupData);
+        console.log(data);
+    }catch(err){
+        console.log(err);
+    }
+}
