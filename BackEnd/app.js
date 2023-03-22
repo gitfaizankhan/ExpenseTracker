84b44341fcf43bc865cadd34ac84d8944611fb11
gitfaizankhan <faizankhaninfo9@gmail.com>
2023-03-23 01:02:58 +0530

    production ready:

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require('fs');
 const dbConnection = require('./utils/dbConnection');
 const userRoute = require('./routes/users');
 const expenseRoute = require("./routes/expense");
@@ -11,12 +12,19 @@ const purchaseRoute = require('./routes/purchase');
 const ForgetPassword = require('./models/forgetpassword');
 const passwordroute = require('./routes/password');
 const FileUrl = require('./models/fileUrlSave');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
+
 
 
 const app = express();
 
-;
+const logStream = fs.createWriteStream('./logs/access.log', { flags: 'a' });
 
+app.use(helmet());
+app.use(compression());
+app.use(morgan('combined', { stream: logStream }));
 
 // test
 app.use(bodyParser.urlencoded({extended:true}));
@@ -28,6 +36,7 @@ app.use('/user', userRoute);
 app.use('/expense', expenseRoute);
 app.use('/purchase', purchaseRoute);
 app.use('/password', passwordroute);
+
 
 User.hasMany(Expense);
 Expense.belongsTo(User) 
@@ -52,6 +61,7 @@ async function syncDB() {
     }
 }
 syncDB();
+
 
 app.listen(3000, ()=>{
     console.log('server is running on http://localhost:3000/')
